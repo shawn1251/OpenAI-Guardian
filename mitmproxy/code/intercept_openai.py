f12a6975a9409e3d5eff9856035a0652caa9db40
shawn1251 <shawn1251@icloud.com>
2025-01-23 00:03:57 -0800

    feat: add interception to response

--- mitmproxy/code/intercept_openai.py
+++ mitmproxy/code/intercept_openai.py
@@ -2,10 +2,14 @@ from mitmproxy import http
 import requests
 import json
 
+is_blocked = False
+
 def request(flow: http.HTTPFlow) -> None:
+    global is_blocked
     if "api.openai.com" in flow.request.pretty_url:
-        #print("Request URL:", flow.request.pretty_url)
-        #print("Request Headers:", flow.request.headers)
+        print("======request======")
+        print("Request URL:", flow.request.pretty_url)
+        print("Request Headers:", flow.request.headers)
         request_body = flow.request.text
         print("Request Body:", request_body)
         try:
@@ -19,18 +23,55 @@ def request(flow: http.HTTPFlow) -> None:
             "content": content})
         
         guardian_response = response.json()
-        print(guardian_response)
+        print("Guardian Response:", guardian_response)
         if guardian_response.get('label') == True:
             flow.response = http.Response.make(
                 403,
                 guardian_response.get("message").encode(),
                 {"Content-Type": "text/plain"}
             )
+            is_blocked = True
 
 
 
 def response(flow: http.HTTPFlow) -> None:
+    global is_blocked
     if "api.openai.com" in flow.request.pretty_url:
+        if is_blocked:
+            is_blocked = False
+            return
+        print("======response======")
         print("Response Status Code:", flow.response.status_code)
         print("Response Headers:", flow.response.headers)
-        print("Response Body:", flow.response.text)
+        response_body = flow.response.text
+        print("Response Body:", response_body)
+        try:
+            data = json.loads(response_body)
+            response_content = data['choices'][0]['message']
+        except (json.JSONDecodeError, KeyError, IndexError) as e:
+            print("Error parsing request body:", e)
+    
+        # this granit guardian service need both user and assistant message
+        request_body = flow.request.text
+        print("Request Body:", request_body)
+        try:
+            data = json.loads(request_body)
+            request_content = data['messages'][0]
+        except (json.JSONDecodeError, KeyError, IndexError) as e:
+            print("Error parsing request body:", e)
+
+        full_content = [request_content, response_content]
+        print("Full conversation", full_content)
+        # Send the "user" + "assistant" message to guardian service
+        response = requests.post("http://guardian:8000/", json={
+            "content": full_content})
+        
+        guardian_response = response.json()
+        print("Guardian Response:", guardian_response)
+        if guardian_response.get('label') == True:
+            flow.response = http.Response.make(
+                403,
+                guardian_response.get("message").encode(),
+                {"Content-Type": "text/plain"}
+            )
+        
